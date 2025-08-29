df904b9420fac0c2cd60a66882f71a059d301e6e
Juan Cruz Elias Martin <108189589+h4che404@users.noreply.github.com>
2025-08-29 18:27:54 -0300

    Fix: Se asigno las tareas al equipo

---
+++ TP3-Condicionales/TP3-Condicionales.py
@@ -0,0 +1,80 @@
+''' ((David))
+1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+'''
+
+'''  ((Nahuel))
+2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+mensaje “Desaprobado”.
+'''
+
+
+'''  ((Agus))
+3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+operador de módulo (%) en Python para evaluar si un número es par o impar.
+'''
+
+''' ((Juan))
+4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+siguientes categorías pertenece:
+● Niño/a: menor de 12 años.
+● Adolescente: mayor o igual que 12 años y menor que 18 años.
+● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+● Adulto/a: mayor o igual que 30 años.
+'''
+
+'''  ((Juan Cruz))
+5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
+(incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
+pantalla el mensaje "Ha ingresado una contraseña correcta"; en caso contrario, imprimir por
+pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: investigue el uso
+de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
+como una lista o un string.
+'''
+
+'''  ((Juan Cruz))
+6) El paquete statistics de python contiene funciones que permiten tomar una lista de números
+y calcular la moda, la mediana y la media de dichos números. 
+
+'''
+
+'''  ((Joaco))
+7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
+termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por
+pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
+pantalla.
+'''
+
+'''  ((Nahuel))
+8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
+dependiendo de la opción que desee:
+1. Si quiere su nombre en mayúsculas. Por ejemplo: PEDRO.
+2. Si quiere su nombre en minúsculas. Por ejemplo: pedro.
+3. Si quiere su nombre con la primera letra mayúscula. Por ejemplo: Pedro.
+El programa debe transformar el nombre ingresado de acuerdo a la opción seleccionada por el
+usuario e imprimir el resultado por pantalla. Nota: investigue uso de las funciones upper(),
+lower() y title() de Python para convertir entre mayúsculas y minúsculas.
+'''
+
+'''  ((Juan))
+9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+por pantalla:
+● Menor que 3: "Muy leve" (imperceptible).
+● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+generalmente no causa daños).
+● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+débiles).
+● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+'''
+
+'''  ((Agus))
+10) Utilizando la información aportada en la siguiente tabla sobre las estaciones del año escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
+del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
+si el usuario se encuentra en otoño, invierno, primavera o verano.
+'''
