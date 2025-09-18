3a3b3d87cced0fc02ae5ff63b50df53553ee2ea6
T-zero12 <juandelx0@gmail.com>
2025-09-18 19:58:54 -0300

    punto1

--- TP5-Listas/TP5-Listas.py
+++ TP5-Listas/TP5-Listas.py
@@ -1,13 +1,15 @@
 #Práctico 5: Listas
 
 '''1) Crear una lista con los números del 1 al 100 que sean múltiplos de 4. Utilizar la función
-range.'''
+range.  Juan'''
 
+multi_de_4 = list(range(4, 101, 4))
+print(multi_de_4)
 
 
 '''2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el
 penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el
-indexing con números negativos!'''
+indexing con números negativos! Juan'''
 
 
 
