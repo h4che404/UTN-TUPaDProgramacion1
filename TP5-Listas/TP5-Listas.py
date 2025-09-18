e5433724c672180f1344217e8d3c90e653a92c71
hero21lb <herotroll21@gmail.com>
2025-09-18 18:52:03 -0300

    Feat: punto 9

--- TP5-Listas/TP5-Listas.py
+++ TP5-Listas/TP5-Listas.py
@@ -55,6 +55,11 @@ b) Reemplazar "fideos" por "tallarines" en la lista del segundo cliente.
 c) Eliminar "pan" de la lista del primer cliente.
 d) Imprimir la lista resultante por pantalla'''
 
+compras = [["pan","leche"],["arroz","fideos","salsa"],["agua"]]
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+print(compras)
 
 
 '''10) Elaborar una lista anidada llamada “lista_anidada” que contenga los siguientes elementos:
